8df992bf7981c164baf9e0043ddff8f50a78209b
Ed Courtney <edcourtney74@gmail.com>
2018-11-18 15:37:15 -0500

    Characters from array made into buttons and displayed on the page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,45 @@
 // VARIABLES ====================================================================
 // Variable that holds an array of characters
-var characters = [];
+var characters = ["Homer", "Marge", "Bart", "Lisa", "Maggie", "Moe", "Barney", "Nelson", "Mr. Burns", 
+"Ned", "Millhouse", "Apu", "Grampa", "Willie", "xxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx"];
 
+// Variable for button that will be created across the page
+var newButton;
 
+// Variable that will hold the URL needed for the specific character search
+var queryURL;
 
 
 // FUNCTIONS ====================================================================
-// Create buttons for characters
+function createButton() {
+
+    // Erase current buttons so duplicates aren't added
+    $("#button-list").empty();
+
+    // Create buttons for characters
     // For loop over characters array
+    for (i = 0; i < characters.length; i++) {              
+        
+        // Creates physical button that displays name of character in array
+        // Creates button tag
+        newButton = $("<button>");
+        // Adds classes from Bootstrap, attributes to add character name, text to button
+        newButton.addClass("btn btn-primary btn-sm mx-1 mt-1").attr("data-name", characters[i]).text(characters[i]);
+        // Add button to HTML
+        $("#character-list").append(newButton);  
+       
+        // Store value in button to add to queryURL for AJAX request
+        
+
+
+        // Remove any non-letters so that the URL 
+
+        // Create HTML button for each character in array on page load
+        queryURL = "https://api.giphy.com/v1/gifs/search?q=simpsons+" + characters[i] + "&api_key=CEnxMFTt8LQl5mNY8uF0N6NKduTtPcgK&limit=10&rating=g+pg"; 
+        
+        console.log(queryURL);
+    }
+}
 
 // Display rating of each GIF
 
@@ -38,5 +70,5 @@ var characters = [];
 
 // MAIN PROCESS================================================================================
 $(document).ready(function () {
-    startGame();
+    createButton();
 });
