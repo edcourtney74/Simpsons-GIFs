c1a297f833aff86ea004e5373e65cc2f406aa92d
Ed Courtney <edcourtney74@gmail.com>
2018-11-18 14:57:39 -0500

    Initial HTML layout complete.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,24 @@
+// VARIABLES ====================================================================
+// Variable that holds an array of characters
+var characters = [];
+
+
+
+
+// FUNCTIONS ====================================================================
+// Create buttons for characters
+    // For loop over characters array
+
+// Display rating of each GIF
+
+
+
+// CLICK FUNCTIONS =============================================================
+// When user clicks a button (grab 10 static, non-animated GIFs from GIPHY):
+    // Returns 10 GIFs from GIPHY - non-animated
+    // Display GIFs on page
+
+// GIF begins or stops animation on GIF button click
 
 
 
