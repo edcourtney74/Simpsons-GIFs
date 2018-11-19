5ba9cbb15b6e1eb8a4f0f97aaa32bec67b75389d
Ed Courtney <edcourtney74@gmail.com>
2018-11-19 08:35:42 -0500

    GIFs displaying when button pressed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,54 +3,119 @@
 var characters = ["Homer", "Marge", "Bart", "Lisa", "Maggie", "Moe", "Barney", "Nelson", "Mr. Burns", 
 "Ned", "Millhouse", "Apu", "Grampa", "Willie", "xxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx"];
 
+// Variable that holds only letters/numbers of character (no spaces, special characters)
+var textNameOnly;
+
 // Variable for button that will be created across the page
 var newButton;
+
+// Variable for search term
+var searchName;
 
 // Variable that will hold the URL needed for the specific character search
 var queryURL;
 
 
+
+
 // FUNCTIONS ====================================================================
 function createButton() {
 
-    // Erase current buttons so duplicates aren't added
+    // Erase current buttons from HTML so duplicates aren't added
     $("#button-list").empty();
 
     // Create buttons for characters
     // For loop over characters array
     for (i = 0; i < characters.length; i++) {              
+    
+        // Create character name with no special characters - will help our URL query
+        textNameOnly = characters[i].replace(/[^A-Z0-9]/ig, "");       
         
         // Creates physical button that displays name of character in array
         // Creates button tag
         newButton = $("<button>");
-        // Adds classes from Bootstrap, attributes to add character name, text to button
-        newButton.addClass("btn btn-primary btn-sm mx-1 mt-1").attr("data-name", characters[i]).text(characters[i]);
+        // Adds classes from Bootstrap, class .gif-selection for on click functioning;
+        // attribute of character name (no spaces), text to button
+        newButton.addClass("btn btn-primary btn-sm mx-1 mt-2 gif-selection").attr("data-name", textNameOnly).text(characters[i]);
         // Add button to HTML
-        $("#character-list").append(newButton);  
-       
-        // Store value in button to add to queryURL for AJAX request
-        
-
-
-        // Remove any non-letters so that the URL 
-
-        // Create HTML button for each character in array on page load
-        queryURL = "https://api.giphy.com/v1/gifs/search?q=simpsons+" + characters[i] + "&api_key=CEnxMFTt8LQl5mNY8uF0N6NKduTtPcgK&limit=10&rating=g+pg"; 
-        
-        console.log(queryURL);
+        $("#character-list").append(newButton);   
     }
 }
 
-// Display rating of each GIF
+// Function to send API request
+function sendRequest() {    
+
+    //Set queryURL variable needed for API request
+    // Also sets limit of 10 and rating of up to PG-13 
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=simpsons+" + searchName + "&api_key=CEnxMFTt8LQl5mNY8uF0N6NKduTtPcgK&limit=10&rating=pg-13"; 
+   
+    // Send AJAX request according to button pressed
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(res) {
+        console.log(res);
+        // Create for loop to go through all 10 items in response array
+        for (let x = 0; x < res.data.length; x++) {
+            
+            // Create div for each GIF
+            // var divGIF = $("<div>");
+
+            // Crete img element to hold GIF
+            var image = $("<img>");
+
+            // Create the still GIF URL for img element
+            var stillGIF = res.data[x].images.fixed_width_still.url;
+            
+            // Create the animated GIF URL for img element
+            var animatedGIF = res.data[x].images.fixed_width.url;
+
+            // Set img url to stillGIF when placed on page
+            image.attr("src", stillGIF);
+
+            // Add stillGIF, animatedGIF to img element
+            image.attr("data-still", stillGIF).attr("data-animate", animatedGIF);
+
+            // Add data-state element to img element, set to still
+            image.attr("data-state", "still");
+
+            // Display image on HTML
+            $("#GIF-display").append(image);
 
 
 
-// CLICK FUNCTIONS =============================================================
-// When user clicks a button (grab 10 static, non-animated GIFs from GIPHY):
-    // Returns 10 GIFs from GIPHY - non-animated
-    // Display GIFs on page
+
+
+            
+        }
+
+    })
+};
+
+
+
+// Click function for buttons created
+$(document).on("click", ".gif-selection", function() {
+    // Retrieves value of "data-name" to use in queryURL
+    searchName = $(this).attr("data-name");
+    console.log("Search name: " + searchName);
+    
+    // Run sendRequest function to get API
+    sendRequest();
+
+});
+    
+
+
+
+       
+
+
 
 // GIF begins or stops animation on GIF button click
+
+// Display rating of each GIF
+
 
 
 
@@ -71,4 +136,5 @@ function createButton() {
 // MAIN PROCESS================================================================================
 $(document).ready(function () {
     createButton();
+     
 });
